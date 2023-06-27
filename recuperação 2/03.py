c33d69cb1836985bc2873186e77c7516a2ec96e8
FranciscaGeovanna <115251617+FranciscaGeovanna@users.noreply.github.com>
2023-06-27 16:36:32 -0300

    Delete recuperação 2 directory

--- recuperação 2/03.py
+++
@@ -1,6 +0,0 @@
-idade = int(input("Digite sua idade para saber a porcentagem do seu desconto: "))
-
-if idade < 21:
-    print ("\nSeu desconto é de 15%")
-else:
-    print("\nSeu desconto é de 10%")
